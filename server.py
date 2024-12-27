5e64f4a176caec183450ab40b4e3bdd092dd8b1c
Olya Yusupova <162722757+olyala05@users.noreply.github.com>
2024-12-27 06:48:24 +0300

    Html Templates Kullanımı

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+movies = ["Movie1","Movie2","Movie3"]
+
 @app.route("/")
 def home_page():
-    return redirect(url_for("welcome_page", name = "Olya"))
-
-@app.route("/welcome<name>")
-def welcome_page(name):
-    return "<p>Welcome {}</p>".format(name)
+    # bu html sayfasının içine bu id ve movies adlı değişken gönderdim
+    return render_template("home_page.html", id = 7, movies = movies)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
